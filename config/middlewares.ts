928ea47ca18de5985f0e58bf865d6b6606e919b7
Tirthankar03 <tirthankar.nath2@gmail.com>
2025-02-18 11:45:35 +0530

    update: cors for hosted client

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,11 +2,26 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://ayna-chat-tn.vercel.app', 'http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      secure: true,
+      sameSite: 'none',
+      httpOnly: true,
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://ayna-chat-server.onrender.com'), // Add this line
+  proxy: true, // Add this line
   app: {
     keys: env.array('APP_KEYS'),
   },
